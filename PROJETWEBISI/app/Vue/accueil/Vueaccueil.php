914211f4fe7dfa4389f288ac9c2e4eb8b03daa9b
jess_nd <jessica.ndiaye@etu.univ-lyon1.fr>
2023-12-06 18:08:20 +0100

    njdss 06/12

---
+++ PROJETWEBISI/app/Vue/accueil/Vueaccueil.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>LIVITYSHOP </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://localhost/livityshop/public/css/accueil.css">
+</head>
+<body>
+
+
+
+<!-- Section Bienvenue -->
+    <div class="content">
+        <article>
+            <h1>Bienvenue!!</h1>
+            <p>Bienvenue sur ISIWeb4Shop. Cliquez sur la liste de gauche pour découvrir notre offre. Nous avons en stock une large gamme de produits.</p>
+            <p>L'ensemble de ce site est ISIWEB4Shop 2017-2028</p>
+        </article>
+
+        <!-- Autres sections de contenu vont ici -->
+    </div>
+
+    <div class = "overlay">
+</body>
+</html>
--- PROJETWEBISI/index.php
+++ PROJETWEBISI/index.php
@@ -1,7 +1,41 @@
 <?php
+$directory = __DIR__;
+$files = scandir($directory);
 
-require 'Controleur/Routeur.php';
+echo "<h2>Contenu du répertoire : $directory</h2>";
+echo "<ul>";
 
-$routeur = new Routeur();
-$routeur->routerRequete();
+foreach ($files as $file) {
+    if ($file != "." && $file != "..") {
+        $path = $directory . DIRECTORY_SEPARATOR . $file;
+        $url = 'http://localhost/livityshop/' . $file;
 
+        echo "<li><a href=\"$url\">$file</a></li>";
+    }
+}
+
+echo "</ul>";
+
+
+
+
+
+// Chemin du répertoire à parcourir
+$directory = 'app/accueil/';
+
+// Ouvrir le répertoire
+$dir = opendir($directory);
+
+// Lire tous les fichiers et répertoires dans le répertoire
+while (($file = readdir($dir)) !== false) {
+    // Exclure les fichiers et répertoires spéciaux
+    if ($file != '.' && $file != '..') {
+        // Afficher le lien vers chaque fichier
+        echo '<a href="' . $directory . $file . '">' . $file . '</a><br>';
+    }
+}
+
+// Fermer le gestionnaire de répertoire
+closedir($dir);
+
+?>
